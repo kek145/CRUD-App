58f908187a3bee4dd9df70f164c8c3a94942fcfe
kek145 <yurabogomolov2004@gmail.com>
2023-07-22 17:42:23 +0300

    UPDATE | Added Entities, DTOs and Responses in Domain layer

---
+++ CRUD/CRUD.Domain/Response/BaseResponse.cs
@@ -0,0 +1,18 @@
+﻿using CRUD.Domain.Enum;
+
+namespace CRUD.Domain.Response
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public string Description { get; set; }
+        public StatusCode StatusCode { get; set; }
+        public T Data { get; set; }
+    }
+
+    public interface IBaseResponse<out T>
+    {
+        string Description { get; }
+        StatusCode StatusCode { get; }
+        T Data { get; }
+    }
+}
