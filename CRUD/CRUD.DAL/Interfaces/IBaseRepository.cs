a8773d3a82c176b6a0edb7082fc62fe6a40cd359
kek145 <yurabogomolov2004@gmail.com>
2023-07-22 18:26:52 +0300

    UPDATE | Added UserRepository

---
+++ CRUD/CRUD.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task CreateAsync(T entity);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task DeleteAsync(int userId);
+
+        Task<T> UpdateAsync(T entity);
+    }
+}
---
+++ CRUD/CRUD.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using CRUD.Domain.Entity;
+using CRUD.DAL.Interfaces;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CRUD.DAL.Repositories
+{
+    public class UserRepository : IBaseRepository<UserEntity>
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task CreateAsync(UserEntity entity)
+        {
+            var connection = new SqlConnection(_configuration.GetConnectionString("MSSQL"));
+            await connection.ExecuteAsync("INSERT INTO [Users](FirstName, LastName, Email) VALUES(@FName, @LName, @Email)", entity);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAllAsync()
+        {
+            var connection = new SqlConnection(_configuration.GetConnectionString("MSSQL"));
+            var users = await connection.QueryAsync<UserEntity>("SELECT * FROM [Users]");
+            
+            return users;
+        }
+
+        public async Task DeleteAsync(int userId)
+        {
+            var connection = new SqlConnection(_configuration.GetConnectionString("MSSQL"));
+            await connection.ExecuteAsync("DELETE FROM [Users] WHERE UserId = @Id", new { Id = userId });
+        }
+
+        public async Task<UserEntity> UpdateAsync(UserEntity entity)
+        { 
+            var connection = new SqlConnection(_configuration.GetConnectionString("MSSQL"));
+            await connection.ExecuteAsync("UPDATE [Users] SET FirstName = @FName, LastName = @LName, Email = @Email", entity);
+
+            return entity;
+        }
+    }
+}
